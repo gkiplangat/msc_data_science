42c251893f5eaa0d7d01c51c62ee8112b887552a
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2025-06-10 23:10:23 +0300

    updates on question

--- fundamentals/02_code.R
+++ fundamentals/02_code.R
@@ -1,3 +1,5 @@
+# Compute summary statistics (mean, median, min, max, etc.) for all numerical variables (e.g., family_income, study_hours_weekly). 
+# What insights do these provide about the data? 
 # For numerical variables
 summary(data %>% select_if(is.numeric))
 
